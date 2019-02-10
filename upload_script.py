174575935f64e002db4362ab1e8b4f495f8e96cb
Sarah Chen <sarahlc888@gmail.com>
2019-02-10 17:20:56 -0500

    init script with one upload

---
+++ upload_script.py
@@ -0,0 +1,10 @@
+import os
+from subprocess import call
+
+# cd to wordpress
+call("pwd")
+os.chdir("/Applications/MAMP/htdocs/wordpress/wp-includes")
+call("pwd")
+
+# make a post with the given parameters
+call("wp post create --from-post=1 --post_title='Testing wp cli FROM PYTHON'", shell=True)
