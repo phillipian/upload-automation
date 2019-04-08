fa8544d0eadae31054d3e22f3f9abb535e49e35c
Sarah Chen <sarahlc888@gmail.com>
2019-04-08 18:26:32 -0400

    function splitting

--- upload_script.py
+++ upload_script.py
@@ -14,20 +14,45 @@ import imgprepare_python_2
 import argparse
 import datetime
 
+
+# PARSE COMMAND
+parser = argparse.ArgumentParser(description='Upload articles from the budget spreadsheet.')
+parser.add_argument('--url', metavar='URL', type=str, nargs='?',
+                    help='url of the budget spreadsheet as it appears in the browser')
+parser.add_argument('--date', metavar='DATE', type=str, nargs='?',
+                    help='date for the paper as it appears in Drobo file directory, ex: 3-29')
+args = parser.parse_args()
+sheet_url = args.url
+paper_week = args.date
+if (sheet_url == None):
+    print('Error: no sheet_url provided')
+    exit(0)
+if (paper_week == None):
+    print('Error: no sheet_url provided')
+    exit(0)
+
 # CONSTANTS
-PATHPREFIX = server_path # PATH to folders
+local_path = '/Volumes/Phillipian/Phillipian/Spring 2019/'+str(helper.replace_dash_w_colon(paper_week))+'/digital'
+server_path = '/wp-photos/'+paper_week+'/'
+ARTICLECAP = 2
+workingdir = os.getcwd()
+
+
+# PATHPREFIX = server_path # PATH to folders # TODO: add this back
+PATHPREFIX = '/Users/sarahchen/Documents/plip-upload/Digital/' # PATH to folders 
 NOPHOTO = 'xxnophotoxx'
 special_photo_credits = ['Archives', 'Courtesy of ']
 category_slugs = {'Arts':'arts', 'Commentary':'commentary', 'Editorial':'editorial', 'Featured Posts':'featured', 'News':'news', 'Sports':'sports', 'The Eighth Page':'eighthpage'}
-sections = ['Sports', 'News', 'Commentary', 'Arts', 'The Eighth Page'] # sections to upload
+# sections = ['Sports', 'News', 'Commentary', 'Arts', 'The Eighth Page'] # sections to upload
+sections = ['News']
 
-existing_writers = [] # import from running list
+# VARS
+fetch_caption = {'':''} # map photo_dir to caption
+fetch_credit = {'':''} # map photo_dir to credit
+existing_writers = [] # import from running list # TODO: get this from wp
 writer_file = open('existing_users.txt','r')
 for line in writer_file:
     existing_writers.append(line[:-1]) # cut off the new line
-
-local_path = '/Volumes/Phillipian/Phillipian/Spring 2019/'+helper.replace_dash_w_colon(paper_week)+'/digital'
-server_path = '/wp-photos/'+paper_week+'/'
 
 def copy_photos_to_server():
     cmd = 'scp -r "'+local_path+'" auto_upload@craig.dreamhost.com:'+server_path+''
@@ -35,10 +60,8 @@ def copy_photos_to_server():
 
 def fetch_photos(sheet_url):
     photo_df = fetch_sheet.get_google_sheet(sheet_url, 'Photo') # fetch image sheet
-    fetch_caption = {'':''} # map photo_dir to caption
-    fetch_credit = {'':''} # map photo_dir to credit
-    if ('Path' in photo_df.columns and 'Caption' in photo_df.columns and 'Photographer' in photo_df.columns):
-        paths = photo_df['Path'].values
+    if ('ImageDir' in photo_df.columns and 'Caption' in photo_df.columns and 'Photographer' in photo_df.columns):
+        paths = photo_df['ImageDir'].values
         captions = photo_df['Caption'].values
         credits = photo_df['Photographer'].values
         if (not (len(paths) == len(captions) and len(captions) == len(credits))):
@@ -46,7 +69,11 @@ def fetch_photos(sheet_url):
             exit(0)
 
         # fill in the dictionaries
-        for i in range(min(len(paths), ARTICLECAP)):
+        for i in range(len(paths)):
+            if (captions[i] == '' or credits[i] == ''):
+                print('missing caption or credit')
+                continue
+
             fetch_caption[paths[i]] = captions[i]
         
             credit = ''
@@ -55,10 +82,14 @@ def fetch_photos(sheet_url):
                     credit = credits[i]
                     break
             if (credit == ''):
+                print("c: " + credits[i])
                 credit = credits[i][0]+'.'+credits[i].split(' ')[1]+'/The Phillipian'
 
             fetch_credit[paths[i]] = credit
-
+        print(fetch_caption)
+        print(fetch_credit)
+    else:
+        print('error: missing col')
 def assign_categories(cur_cat_str, article_txt, headline):
     """produce the category string"""
     cat_string = cur_cat_str
@@ -77,43 +108,40 @@ def fetch_writer_id(writer_str):
     writer_login = helper.remove_spaces_commas(writer_str).lower()
     workingdir = os.getcwd()
     os.chdir("/Applications/MAMP/htdocs/wordpress/wp-includes") # TODO: fix
+    
+    try:
+        cmd = 'wp user get --field=ID ' + writer_login
+        writer_id = check_output(cmd, shell=True)
+    except:
+        cmd = 'wp user create ' + writer_login + ' ' + writer_login + "@craig.dreamhost.com --display_name='"+writer+"' --porcelain" 
+        writer_id = check_output(cmd, shell=True)
+    
+    """
     if (writer_str in existing_writers): # TODO: can you acces it?
-        cmd = 'wp user get --ssh=automatic_upload@craig.dreamhost.com --field=ID ' + writer_login
+        cmd = 'wp user get --field=ID ' + writer_login
+        # cmd = 'wp user get --ssh=automatic_upload@craig.dreamhost.com --field=ID ' + writer_login
     else:
-        cmd = 'wp user create ' + writer_login + ' ' + writer_login + "@craig.dreamhost.com --ssh=automaticupload@craig.dreamhost.com --display_name='"+writer+"' --porcelain" 
+        # cmd = 'wp user create ' + writer_login + ' ' + writer_login + "@craig.dreamhost.com --ssh=automaticupload@craig.dreamhost.com --display_name='"+writer+"' --porcelain" 
+        cmd = 'wp user create ' + writer_login + ' ' + writer_login + "@craig.dreamhost.com --display_name='"+writer+"' --porcelain" 
         existing_writers.append(writer)
-    writer_id = check_output(cmd, shell=True)
+    """
+    # writer_id = check_output(cmd, shell=True)
     os.chdir(workingdir)
     return writer_id
 
 
-# PARSE COMMAND
-parser = argparse.ArgumentParser(description='Upload articles from the budget spreadsheet.')
-parser.add_argument('--url', metavar='URL', type=str, nargs='?',
-                    help='url of the budget spreadsheet as it appears in the browser')
-parser.add_argument('--date', metavar='DATE', type=str, nargs='?',
-                    help='date for the paper as it appears in Drobo file directory, ex: 3-29')
-args = parser.parse_args()
-sheet_url = args.url
-paper_week = args.date
-if (sheet_url == None):
-    print('Error: no sheet_url provided')
-    exit(0)
-if (paper_week == None):
-    print('Error: no sheet_url provided')
-    exit(0)
-
 # COPY PHOTOS OVER TO SERVER
-copy_photos_to_server()
+# copy_photos_to_server() # TODO: add this back
 
 # FETCH PHOTOS
 fetch_photos(sheet_url)
-
+print(fetch_caption)
 # FETCH ARTICLES
 for s in sections:
-    section_df = fetch_sheet.get_google_sheet(sheet_url, s) # fetch sheet dataframe
-
-    helper.check_columns(section_df, ['Link','ImageDir','Headline','Writer','Featured?','Upload?']) # confirm that required columns are present
+    print(s)
+    # fetch and verify sheet dataframe content
+    section_df = fetch_sheet.get_google_sheet(sheet_url, s) 
+    helper.check_columns(section_df, ['Link','ImageDir','Headline','Writer','Featured?','Upload?']) 
 
     article_urls = section_df['Link'].values
     headlines = section_df['Headline'].values
@@ -125,67 +153,59 @@ for s in sections:
     if (not (len(article_urls) == len(img_names) and len(headlines) == len(img_names) and len(writers) == len(img_names) and len(statuses) == len(img_names))):
         print('error: '+s+' budget columns not the same length') # I don't think this ever happens but
         exit(0)
-
-    # main category
-    category_slug = category_slugs[s]
+    
+    category_slug = category_slugs[s] # main category
 
     # loop through articles and upload them
     for i in range(min(ARTICLECAP,len(article_urls))):
-        # load and check fields
-        status = statuses[i]
 
-        if (status != 'yes'):
+        if (statuses[i].lower() != 'yes'):
+            print('  do not upload')
             continue # only upload finished articles -- skip all that are not marked
-
+        
+        # load and check fields
         article_url = article_urls[i]
         headline = headlines[i]
         img_name = img_names[i] # directory name
         writer = writers[i]
         featured = featured_posts[i]
-
         helper.check_content([article_url, headline, img_name, writer])
 
-        # progress string
-        print('Uploading: ' + headline + ' from ' + s) 
+        print('Uploading: ' + headline + ' from ' + s) # progress string
 
-        # fetch article text
-        article_txt = fetch_document.get_google_doc(article_url) 
-
-        # assign categories and subcategories
-        category_string = assign_categories(category_slug, article_txt, headline)
-
-        # fetch writer id number, or create user if writer does not exist
-        writer_id = fetch_writer_id(writer)
+        article_txt = fetch_document.get_google_doc(article_url) # fetch article text
+        category_string = assign_categories(category_slug, article_txt, headline) # assign categories and subcategories
+        writer_id = fetch_writer_id(writer) # fetch writer id number, or create user if writer does not exist
         
-        # fetch image 
-        num_imgs = 1
-        if (len(img_name.split(' ')) > 1):
-            print('multiple images! # = '+img_name.split(' ')[1])
-            num_imgs = img_name.split(' ')[1]
-
+        # fetch image(s)
+        # There can be multiple img_names in this field
         if (NOPHOTO not in img_name and img_name != ''):
-            img_dir = PATHPREFIX + s + '/' + img_name
-            imgs = os.listdir(img_dir) 
-            ind = 0
-            while (imgs[ind][0] == '.'): # skip hidden directories
-                ind += 1
+            for name in img_name.split(' '):
+                img_dir = PATHPREFIX + s + '/' + name
+                imgs = os.listdir(img_dir) 
+                ind = 0
+            
+                while (imgs[ind][0] == '.'): # skip hidden directories ('.anything')
+                    ind += 1
 
-            for j in range(num_imgs):
-                # the image to upload
-                img = img_dir+'/'+imgs[ind+j] 
+                # fetch and compress image
+                img = img_dir+'/'+imgs[ind] 
                 img = imgprepare_python_2.compress_img(img, 30) # TODO: use imgprepare
 
                 # upload the image to the media library
                 os.chdir("/Applications/MAMP/htdocs/wordpress/wp-includes")
-                cmd = 'wp media import '+img+' --ssh=automatic_upload@craig.dreamhost.com --porcelain | xargs -I {} wp post list --post__in={} --field=url --ssh=automatic_upload@craig.dreamhost.com --post_type=attachment'
+                cmd = 'wp media import '+img+' --porcelain | xargs -I {} wp post list --post__in={} --field=url --post_type=attachment'
+                # cmd = 'wp media import '+img+' --ssh=automatic_upload@craig.dreamhost.com --porcelain | xargs -I {} wp post list --post__in={} --field=url --ssh=automatic_upload@craig.dreamhost.com --post_type=attachment'
                 img_url = check_output(cmd, shell=True)
                 img_url = helper.media_url_to_img_url(img_url,imgs[ind])
                 os.chdir(workingdir)
 
                 # generate short code for image, prepend to article content
-                
-                image_txt = imgprepare_python_2.img_for_post_content(img_url, fetch_caption[img_name], fetch_credit[img_name]) 
+                # TODO: catch error if name is not in keys
+                if name not in fetch_caption.keys() or name not in fetch_credit.keys():
+                    print('error: imageDir not found in photo budget')
 
+                image_txt = imgprepare_python_2.img_for_post_content(img_url, fetch_caption[name], fetch_credit[name]) 
                 helper.prepend(article_txt, image_txt)
         
         # fix headlines for each section
@@ -197,16 +217,16 @@ for s in sections:
         # if featured article, make timestamp yesterday and add to the featured category
         more_options = ''
         if featured == 'yes':
-            post_timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            dt = datetime
+            post_timestamp = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             more_options += "--post_date='"+post_timestamp+"'"
             category_string += ',featured'
 
-        # make a post with the given parameters
-        # cd to wordpress (test code only)
-        os.chdir("/Applications/MAMP/htdocs/wordpress/wp-includes")
-        # TODO: make status draft for the real site
+        # POST WITH GIVEN PARAMETERS
+        os.chdir("/Applications/MAMP/htdocs/wordpress/wp-includes") # cd to wordpress (test code only)
         # TODO: check if category needs quotes around it
-        cmd = "wp post create "+ workingdir +"/"+ article_txt + " --ssh=automaticupload@craig.dreamhost.com --post_category="+ category_string +" --post_status=publish --post_title='"+ headline +"' --porcelain --post_author="+ writer_id + ' ' + more_options 
+        # cmd = "wp post create "+ workingdir +"/"+ article_txt + " --ssh=automaticupload@craig.dreamhost.com --post_category="+ category_string +" --post_status=draft --post_title='"+ headline +"' --porcelain --post_author="+ writer_id + ' ' + more_options 
+        cmd = "wp post create "+ workingdir +"/"+ article_txt + " --post_category="+ category_string +" --post_status=publish --post_title='"+ headline +"' --porcelain --post_author="+ writer_id + ' ' + more_options 
         post_id = check_output(cmd, shell=True)
 
         os.chdir(workingdir)
