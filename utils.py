1db153ceef8c99de287c28cee5e1aa6cd86b5686
plipdigital <plipdigital@craig.dreamhost.com>
2020-01-13 21:46:26 -0800

    added more to utils

--- utils.py
+++ utils.py
@@ -42,11 +42,57 @@ def fix_authors():
     '''
     Usage: deal with previous "and" authors that are now bad
     '''
-    user_command = 'wp user list --role=author --fields=ID'
-    user_id_list = subprocess.check_output(user_command, shell=True).split('\n')
+    #user_command = 'wp user list --role=author --fields=ID'
+    #user_id_list = subprocess.check_output(user_command, shell=True).split('\n')
     name_list = subprocess.check_output('wp user list --role=author --fields=display_name', shell=True).split('\n')
-    info_list = zip(user_id_list, name_list)
-    print(info_list)
+    #info_list = zip(user_id_list, name_list)
+    fix_name_list = []
+    for user in name_list:
+        first_user = None
+        second_user = None
+        if ' and ' in user.lower():
+            first_user = user.split(' and ')[0]
+            second_user = user.split(' and ')[1]
+
+        elif ' Translated by ' in user:
+            first_user = user.split(' Translated by ')[0]
+            second_user = user.split(' Translated by ')[1]
+        else:
+           
+            continue
+        if first_user not in fix_name_list and first_user not in name_list:
+            fix_name_list.append(first_user)
+        if second_user not in fix_name_list and second_user not in name_list:
+            fix_name_list.append(second_user)
+    print(fix_name_list)
+     
+def fix_posts():
+    '''
+    Usage: reassign posts from broken author usernames with ands to their respective authors
+    '''
+    fix_post_list = []
+
+    post_id_list = subprocess.check_output('wp post list --field=ID', shell=True).split('\n') 
+
+    for post_id in post_id_list:
+        author = subprocess.check_output('wp post get {} --field=post_author'.format(post_id), shell=True).strip()
+        author_name = subprocess.check_output('wp user get {} --field=display_name'.format(author), shell=True)).strip()
+        if 'The Phillipian' in author_name:
+            continue
+        if ' and ' in author_name:
+            author_one = author_name.split(' and ')[0]
+            author_two = author_name.split(' and ')[1]
+        elif ', ' in author_name:
+            author_one = author_name.split(', ')[0]
+            author_two = author_name.split(', ')[1]
+        subprocess.call('wp co-authors-plus add-coauthors --coauthor={} --post_id={}'.format(author_one, post_id), shell=True)
+        subprocess.call('wp co-authors-plus add-coauthors --coauthor={} --post_id={}'.format(author_two, post_id), shell=True)
+        
+        
+
+        
+
+fix_posts()
 
 def fix_jeffreys_life():
     raise NotImplementedError
