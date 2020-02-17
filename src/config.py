3345cdf493b282f467f7338fc69bdbf3e832a0a4
Sarah Chen <sarahlc888@gmail.com>
2020-02-17 17:42:56 -0500

    update config

--- src/config.py
+++ src/config.py
@@ -1,18 +1,16 @@
-# SERVER
-wp_install_dir = '/home/plipdigital/phillipian.net'
 server_name = 'plipdigital@phillipian.net/home/plipdigital/phillipian.net' 
 
-# CONSTANTS
+# PATHS
+# local
 local_article_path = 'articles/'
-server_article_homedir = '/home/plipdigital/temp_articles/' # path to articles on the server
-# path to articles on the server (same as from local script + articles/)
-server_article_path = server_article_homedir+local_article_path
-
-
-# IMG CONSTANTS
 local_img_path = '/Users/jzpan/digital/' # TODO: path to photos in the docker image / local computer
+# server
+server_article_homedir = '/home/plipdigital/temp_articles/' # path to article dir/.. on the server
+server_article_path = server_article_homedir+local_article_path # path to articles on the server 
+wp_install_dir = '/home/plipdigital/phillipian.net' # location to run remote upload script
 server_img_path = '/home/plipdigital/wp-photos/' # path to photos on the server, should contain date folders
 
+# CONTENT
 NOPHOTO = 'nophoto'
 special_photo_credits = ['Archives', 'Courtesy of ']
 category_slugs = {'Arts':'arts', 'Commentary':'commentary', 'Editorial':'editorial', 'Featured Posts':'featured', 'News':'news', 'Sports':'sports', 'The Eighth Page':'eighthpage', 'Multilingual':'multilingual', 'Editorial':'editorial'}
